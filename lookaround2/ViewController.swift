7b082a5d02f8e9a88c55b89d6229170abacc4097
wangela <wangela@users.noreply.github.com>
2017-10-21 01:45:09 -0700

    Initial Commit

---
+++ lookaround2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lookaround2
+//
+//  Created by Angela Yu on 10/21/17.
+//  Copyright © 2017 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
