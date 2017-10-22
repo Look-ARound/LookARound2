2015f8e6615a658b856c05994d4bf6f2a9ba18aa
Siji <rachtom89@gmail.com>
2017-10-22 12:47:30 -0700

    Model updates and database support

---
+++ lookaround2/Models/List.swift
@@ -0,0 +1,52 @@
+//
+//  List.swift
+//  LookARound
+//
+//  Created by Siji Rachel Tom on 10/21/17.
+//  Copyright © 2017 LookARound. All rights reserved.
+//
+
+import Foundation
+
+var firebaseListIDKey = "listID"
+var firebaseListNameKey = "listName"
+var firebaseCreatedByUserIDKey = "createdByUserID"
+var firebasePlaceIDArrayKey = "placeIDs"
+
+
+class List: NSObject {
+    var id: UUID!
+    var name: String!
+    var createdByUserID: UUID!
+    var placeIDs = [UUID]()
+    
+    init(listID : String!, dictionary: [String : AnyObject?]!) {
+        self.id = UUID(uuidString: listID)
+        self.name = dictionary[firebaseListNameKey] as! String
+        
+        let userIDString = dictionary[firebaseCreatedByUserIDKey] as! String
+        self.createdByUserID = UUID(uuidString: userIDString)
+        
+        if let placeIDStrings = dictionary[firebasePlaceIDArrayKey] as? [String] {
+            for placeStr in placeIDStrings {
+                self.placeIDs.append(UUID(uuidString: placeStr)!)
+            }
+        }
+    }
+    
+    func firebaseRepresentation() -> NSDictionary {
+        let dict = NSMutableDictionary()
+        dict[firebaseListNameKey] = self.name
+        dict[firebaseCreatedByUserIDKey] = self.id.uuidString
+        
+        // Change placeIDs to UUID strings
+        var placeIDStrings = [String]()
+        for placeID in self.placeIDs {
+            placeIDStrings.append(placeID.uuidString)
+        }
+        
+        dict[firebasePlaceIDArrayKey] = placeIDStrings
+        
+        return dict as NSDictionary
+    }
+}
